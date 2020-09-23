2b0de5662c276592e236605fd9b79d32b7011f57
Nigel Willoughby <nige.willoughby@gmail.com>
2020-09-23 11:04:30 +0930

    simplified package list. simplified code for package citation tables and changed to sessioninfo package (instead of devtools). Hashed out setup options that are not always relevant.

---
+++ 0030_report.R
@@ -0,0 +1,47 @@
+## ----setup--------------------------------------------------------------------
+
+  source("0010_setup.R")
+
+
+
+## ----setupCode, code = readLines("0010_Setup.R"), echo = TRUE, eval = FALSE----
+## NA
+
+
+## ---- code = readLines(knitr::purl(knitr::current_input(),documentation=1)), echo = TRUE, eval = FALSE----
+## 
+## # From
+## # https://github.com/yihui/knitr/issues/332
+## # https://stackoverflow.com/questions/36868287/purl-within-knit-duplicate-label-error
+## 
+
+
+## ----packages-----------------------------------------------------------------
+
+  kable(tibble(package = packages) %>%
+          dplyr::mutate(citation = paste0("@R-",package)) %>%
+          dplyr::left_join(as_tibble(sessioninfo::package_info(include_base = TRUE))) %>%
+          dplyr::select(package,citation,loadedversion,date,source)
+        , caption = "R [@R-base] packages used in the production of this report" 
+        )
+
+
+
+## ----sessionInfoPackages------------------------------------------------------
+  
+  as_tibble(sessioninfo::package_info()) %>%
+    dplyr::filter(!attached) %>%
+    dplyr::select(package,loadedversion,date,source) %>%
+    kable(caption = "@R-base packages required to produce this report but not explicitly attached")
+  
+
+
+## ----sessionInfoPlatform------------------------------------------------------
+  
+  session_info()$platform %>%
+    tibble::enframe(name = "setting", value = "value") %>%
+    tidyr::unnest() %>%
+    kable() %>%
+    kable_styling()
+  
+
